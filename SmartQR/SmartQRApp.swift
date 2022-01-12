d09d3b7a505cc98bf37515a34300cbe0d3fd8005
atmeed <leo.zemtsoff@gmail.com>
2022-01-12 16:37:05 +0300

    Initial Commit

---
+++ SmartQR/SmartQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartQRApp.swift
+//  SmartQR
+//
+//  Created by Leonid Zemtsov on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
