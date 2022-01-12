e194d37bfcc25a13555c538b261c9cf07cc41ffa
atmeed <leo.zemtsoff@gmail.com>
2022-01-12 16:40:13 +0300

    Create main View

---
+++ SmartQR/GeneratorQR.swift
@@ -0,0 +1,20 @@
+//
+//  GeneratorQR.swift
+//  SmartQR
+//
+//  Created by Leonid Zemtsov on 12.01.2022.
+//
+
+import SwiftUI
+
+struct GeneratorQR: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GeneratorQR_Previews: PreviewProvider {
+    static var previews: some View {
+        GeneratorQR()
+    }
+}
---
+++ SmartQR/ScannerQR.swift
@@ -0,0 +1,20 @@
+//
+//  ScannerQR.swift
+//  SmartQR
+//
+//  Created by Leonid Zemtsov on 12.01.2022.
+//
+
+import SwiftUI
+
+struct ScannerQR: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScannerQR_Previews: PreviewProvider {
+    static var previews: some View {
+        ScannerQR()
+    }
+}
