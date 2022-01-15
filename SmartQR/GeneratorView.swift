bf34fc19af5e14853ba801e29e8081932401c98a
atmeed <leo.zemtsoff@gmail.com>
2022-01-15 16:25:26 +0300

    Add delete all history generate QR codes

--- SmartQR/GeneratorView.swift
+++ SmartQR/GeneratorView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 //Строчки в Истории
 struct HistoryRow: View {
     
-    var QR: StringToQR
+    var QR: QR
     
     var body: some View {
         HStack {
@@ -24,7 +24,7 @@ struct HistoryRow: View {
 
 //Просмотр старого QR
 struct HistoryView: View {
-    var QR: StringToQR
+    var QR: QR
     
     var body: some View {
         VStack {
@@ -36,25 +36,39 @@ struct HistoryView: View {
 
 struct GeneratorView: View {
     
-    var historyQR = ["Test", "Text", "Free"]
+    @State private var historyQR = ["Test", "Text", "Free", "Pee"]
+    
     
     var body: some View {
         NavigationView {
             VStack {
+                //Последний QR
                 
                 
                 //История создания
                 Form {
-                    Section(header: Text("История")) {
-                        List {
-                            ForEach(historyQR, id: \.self) { qr in
-                                NavigationLink(destination: HistoryView(QR: StringToQR(name: qr))) {
-                                    HistoryRow(QR: StringToQR(name: qr))
+                    if historyQR.count > 0 { //Удаление истории приводит к удалению всего View
+                        Section(header: Text("История")) {
+                            List {
+                                ForEach(historyQR, id: \.self) { qr in
+                                    NavigationLink(destination: HistoryView(QR: StringToQR(name: qr))) {
+                                        HistoryRow(QR: StringToQR(name: qr))
+                                    }
                                 }
+                            }
+                            
+                            //Удаление всей истории QR
+                            HStack{
+                                Button("Удалить всю истроию") {
+                                    self.historyQR = []
+                                }
+                                .foregroundColor(.red)
+                                
                                     
                             }
                         }
                     }
+                    
                 }
             }.navigationTitle("Создание QR")
             
