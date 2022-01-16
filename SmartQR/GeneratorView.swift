4b02a0fa24e1ff9afde02237f79006ca709026e4
atmeed <leo.zemtsoff@gmail.com>
2022-01-16 03:31:30 +0300

    Fix share Button

--- SmartQR/GeneratorView.swift
+++ SmartQR/GeneratorView.swift
@@ -22,27 +22,81 @@ struct HistoryRow: View {
     }
 }
 
+//Поделитсья
+struct ShareSheet: UIViewControllerRepresentable {
+
+    let activityItems: [Any]
+    let applicationActivities: [UIActivity]?
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ShareSheet>) -> UIActivityViewController {
+        return UIActivityViewController(activityItems: activityItems,
+                                        applicationActivities: applicationActivities)
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController,
+                                context: UIViewControllerRepresentableContext<ShareSheet>) {
+
+    }
+}
+
 //Просмотр старого QR
 struct HistoryView: View {
-    var QR: QR
+    let QR: QR
+    
+    @State private var isShowingSharingSheet = false
+    
     
     var body: some View {
         VStack {
-            Image(uiImage: QR.image)
+            Form {
+                HStack {
+                    Spacer()
+                    Text("\(QR.name)").font(.headline)
+                    Spacer()
+                }
+                Image(uiImage: QR.image)
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                HStack {
+                    Spacer()
+                    Button("Поделиться") {
+                        self.isShowingSharingSheet = true
+                    }.sheet(isPresented: $isShowingSharingSheet, content: {
+                        ShareSheet(activityItems: [QR.image], applicationActivities: nil)
+                    })
+                    Spacer()
+                }
+            }
         }
     }
 }
 
 
+
+//BODY
 struct GeneratorView: View {
     
-    @State private var historyQR = ["Test", "Text", "Free", "Pee"]
+    @State private var historyQR = ["Test", "Text", "Free", "Pe"]
     
     
     var body: some View {
         NavigationView {
             VStack {
                 //Последний QR
+                
+                
+                
+                
+                
+                
+                
+                
+                //Создание нового QR
+                
+                
+                
+                
                 
                 
                 //История создания
